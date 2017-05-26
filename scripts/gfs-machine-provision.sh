c194f15031ff64485bb261643f17d6b6bcaeeb66
chris vugrinec <chris@mbp-chris.home>
2017-05-26 04:01:50 +0200

    remove ssh

--- scripts/gfs-machine-provision.sh
+++ scripts/gfs-machine-provision.sh
@@ -3,8 +3,8 @@ sshKey="$1 $2 $3"
 apt-get -y update
 apt-get -y install glusterfs-server
 #ssh-keygen -t rsa -f  ~/.ssh/id_rsa -P ""
-mkdir ~/.ssh
-echo "$sshKey">~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
+#mkdir ~/.ssh
+#echo "$sshKey">~/.ssh/authorized_keys
+#chmod 600 ~/.ssh/authorized_keys
 
 
