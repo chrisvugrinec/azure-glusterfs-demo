a397b4deac8272d92f0c47f71cbd44fff0cc27df
chris vugrinec <chris@mbp-chris.home>
2017-05-26 02:43:54 +0200

    added sshKey

--- scripts/gfs-machine-provision.sh
+++ scripts/gfs-machine-provision.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+sshKey=$1
 apt-get -y update
 apt-get -y install glusterfs-server
+ssh-keygen -t rsa -f  ~/.ssh/id_rsa -P ""
+echo $sshKey > ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
 
 
