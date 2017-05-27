b1d3ffcfd4682b9cad0c4330e87491b4d71a65a8
chris vugrinec <chris@mbp-chris.home>
2017-05-27 18:27:37 +0200

    fix

--- scripts/gluster-volume.sh
+++ scripts/gluster-volume.sh
@@ -7,27 +7,26 @@
 # gluster vol create dist-vol glusterdemo-chris-1VM:/mnt/brick1/dist-vol glusterdemo-chris-2VM:/mnt/brick2/dist-vol 
 #
 
-for host in $(cat /tmp/tmp_hosts | grep VM | sed 's/^.*[\ ]//')
-do
-  gluster peer detach $host
-done
-
-# prep
-for host in $(cat /tmp/tmp_hosts | grep VM | sed 's/^.*[\ ]//')
-do
-  gluster peer probe $host
-  nr=$(echo $host | sed 's/^.*[-]//' | sed 's/VM//')
-  command+=" "$host:/mnt/brick$nr/dist-vol
-done
-
-echo "command needs to be: gluster vol create dist-vol $command"
-
-# creating the gluster cluster
-gluster vol create dist-vol $command
-gluster volume start dist-vol
-
 mkdir -p /mnt/gluster
 hostname=`hostname`
+nr=$(echo $hostname | sed 's/^.*[-]//' | sed 's/VM//')
+
+# creating the gluster cluster, only on 1st machine needed
+if [[ $nr == "1" ]]
+then
+  # prep
+  for host in $(cat /tmp/tmp_hosts | sort -n | grep VM | sed 's/^.*[\ ]//')
+  do
+    gluster peer probe $host
+    nr=$(echo $host | sed 's/^.*[-]//' | sed 's/VM//')
+    command+=" "$host:/mnt/brick$nr/dist-vol
+  done
+  echo "command to be executed: gluster vol create dist-vol $command"
+
+  gluster vol create dist-vol $command
+  gluster volume start dist-vol
+fi
+
 
 # Mounting the gluster volume
 mount -t glusterfs $hostname:/dist-vol /mnt/gluster
